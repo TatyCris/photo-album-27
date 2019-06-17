7218e99d433817f8c70673de82cc46742b1bc088
TATY <tatyccsilva@gmail.com>
2019-06-17 16:03:58 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import './App.css';
 import AlbumsListContainer from './components/AlbumListContainer';
-
+import { Route }  from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <AlbumsListContainer />
+          <Route exact path="/" component={AlbumsListContainer} />
         </header>
       </div>
     );
